9131dd8169c79bf7ed18eea46ccb99097bef7fa2
Evengarr <Ravasect@gmail.ru>
2022-01-22 18:34:28 +0300

    Это тестовая ветка, для отработки групповой разработки на GitHub

--- src/main/java/ru/project/musicbandsearch/MusicBandSearchApplication.java
+++ src/main/java/ru/project/musicbandsearch/MusicBandSearchApplication.java
@@ -2,11 +2,13 @@ package ru.project.musicbandsearch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import ru.project.musicbandsearch.evengarr_test.EvengarrSayHelloTeam;
 
 @SpringBootApplication
 public class MusicBandSearchApplication {
 
     public static void main(String[] args) {
+        EvengarrSayHelloTeam.sayHello();
         SpringApplication.run(MusicBandSearchApplication.class, args);
     }
 
---
+++ src/main/java/ru/project/musicbandsearch/evengarr_test/EvengarrSayHelloTeam.java
@@ -0,0 +1,7 @@
+package ru.project.musicbandsearch.evengarr_test;
+
+public class EvengarrSayHelloTeam {
+    public static void sayHello(){
+        System.out.println("Я говорю: Привет, группа!");
+    }
+}
